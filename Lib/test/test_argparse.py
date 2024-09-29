d8a5dc85619bcf5f553a809090abe1a57e4a70cb
Serhiy Storchaka <storchaka@gmail.com>
2024-09-29 14:15:03 +0300

    gh-61011: Fix inheriting mutually exclusive groups from parent parsers
    Co-authored-by: Dougal J. Sutherland

--- Lib/test/test_argparse.py
+++ Lib/test/test_argparse.py
@@ -2904,6 +2904,36 @@ class TestParentParsers(TestCase):
     def test_wrong_type_parents(self):
         self.assertRaises(TypeError, ErrorRaisingArgumentParser, parents=[1])
 
+    def test_mutex_groups_parents(self):
+        parent = ErrorRaisingArgumentParser(add_help=False)
+        g = parent.add_argument_group(title='g', description='gd')
+        g.add_argument('-w')
+        g.add_argument('-x')
+        m = g.add_mutually_exclusive_group()
+        m.add_argument('-y')
+        m.add_argument('-z')
+        parser = ErrorRaisingArgumentParser(parents=[parent])
+
+        self.assertRaises(ArgumentParserError, parser.parse_args,
+            ['-y', 'Y', '-z', 'Z'])
+
+        parser_help = parser.format_help()
+        progname = self.main_program
+        self.assertEqual(parser_help, textwrap.dedent('''\
+            usage: {}{}[-h] [-w W] [-x X] [-y Y | -z Z]
+
+            optional arguments:
+              -h, --help  show this help message and exit
+
+            g:
+              gd
+
+              -w W
+              -x X
+              -y Y
+              -z Z
+        '''.format(progname, ' ' if progname else '' )))
+
 # ==============================
 # Mutually exclusive group tests
 # ==============================
